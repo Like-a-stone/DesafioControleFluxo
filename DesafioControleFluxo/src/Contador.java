d4c772f400f1896ae626713b586b2789a32c95ae
Lucas <Lucas.Sampaio@jala.university>
2024-06-03 10:42:30 -0300

    Incluido lógica para contagem

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -14,14 +14,20 @@ public class Contador {
 			contar(parametroUm, parametroDois);
 		
 		}catch (ParametrosInvalidosException exception) {
-			//imprimir a mensagem: O segundo parâmetro deve ser maior que o primeiro
+            System.out.println(exception.getMessage());
 		}
-		
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+        if( parametroUm > parametroDois)
+        throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
 		
 		int contagem = parametroDois - parametroUm;
+
 		//realizar o for para imprimir os números com base na variável contagem
+        for (int i = 1; i <= contagem; i++) {
+            System.out.printf("Imprimindo o número %d \n", i);
+            parametroUm++;
+        }
 	} 
 }
